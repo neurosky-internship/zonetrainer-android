5dae25b7cf04e29284ed3812cd549154bc2dc995
yjyoon-dev <yjyoon.dev@gmail.com>
2022-09-06 13:30:33 +0900

    feat: implement Splash

---
+++ app/src/main/java/dev/yjyoon/neurosky/ui/splash/SplashScreen.kt
@@ -0,0 +1,22 @@
+package dev.yjyoon.neurosky.ui.splash
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("SplashScreen")
+    }
+}
