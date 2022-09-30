977940a732179381d207ccf3307cd64fb0e62cf7
yjyoon-dev <yjyoon.dev@gmail.com>
2022-10-01 01:20:15 +0900

    feat: define DM Sans typography

--- app/src/main/java/com/neurosky/zonetrainer/ui/theme/Type.kt
+++ app/src/main/java/com/neurosky/zonetrainer/ui/theme/Type.kt
@@ -2,33 +2,31 @@ package com.neurosky.zonetrainer.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.neurosky.zonetrainer.R
 
-// Set of Material typography styles to start with
+val DmSans = FontFamily(
+    Font(R.font.dmsans_bold, FontWeight.Bold),
+    Font(R.font.dmsans_medium, FontWeight.Medium),
+    Font(R.font.dmsans_regular, FontWeight.Normal)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+    headlineLarge = TextStyle(
+        fontFamily = DmSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleLarge = TextStyle(
+        fontFamily = DmSans,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
     )
-    */
 )
