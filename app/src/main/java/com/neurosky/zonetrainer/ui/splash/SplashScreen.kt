98d9d23252282d8193b1d500f6fe7bee4c8af056
yjyoon-dev <yjyoon.dev@gmail.com>
2022-09-30 17:13:14 +0900

    feat: SplashScreen UI

--- app/src/main/java/com/neurosky/zonetrainer/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/neurosky/zonetrainer/ui/splash/SplashScreen.kt
@@ -1,22 +1,53 @@
 package com.neurosky.zonetrainer.ui.splash
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.neurosky.zonetrainer.R
+import com.neurosky.zonetrainer.ui.theme.NeuroskyTheme
 
 @Composable
 fun SplashScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
-        contentAlignment = Alignment.Center
+            .background(MaterialTheme.colorScheme.background)
+            .padding(16.dp)
     ) {
-        Text("SplashScreen")
+        Image(
+            painter = painterResource(id = R.drawable.img_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(256.dp)
+        )
+        Image(
+            painter = painterResource(id = R.drawable.img_brand),
+            contentDescription = null,
+            modifier = Modifier
+                .align(
+                    Alignment.BottomCenter
+                )
+                .width(96.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    NeuroskyTheme {
+        SplashScreen()
     }
 }
