c668634df4d8a7ed70ae89372664a6a615e6f4f5
yjyoon-dev <yjyoon.dev@gmail.com>
2022-10-01 03:39:34 +0900

    fix: theme container color

--- app/src/main/java/com/neurosky/zonetrainer/ui/theme/Theme.kt
+++ app/src/main/java/com/neurosky/zonetrainer/ui/theme/Theme.kt
@@ -4,21 +4,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val LightColorScheme = lightColorScheme(
     primary = NeuroBlue,
     onPrimary = White,
-    primaryContainer = NeuroBlue.copy(alpha = 0.33f),
+    primaryContainer = NeuroBlue.copy(alpha = 0.12f),
     onPrimaryContainer = NeuroBlue,
     secondary = NeuroPurple,
     onSecondary = White,
-    secondaryContainer = NeuroPurple.copy(alpha = 0.33f),
+    secondaryContainer = NeuroPurple.copy(alpha = 0.12f),
     onSecondaryContainer = NeuroPurple,
     tertiary = NeuroRed,
     onTertiary = White,
-    tertiaryContainer = NeuroRed.copy(alpha = 0.33f),
+    tertiaryContainer = NeuroRed.copy(alpha = 0.12f),
     onTertiaryContainer = NeuroRed,
     background = White,
     onBackground = Black,
@@ -33,7 +32,7 @@ fun NeuroTheme(
     val systemUiController = rememberSystemUiController()
     SideEffect {
         systemUiController.setSystemBarsColor(
-            color = Color.Transparent,
+            color = White,
             darkIcons = true,
             isNavigationBarContrastEnforced = false,
         )
