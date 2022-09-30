d879356612bf289bf9a6057a4d93101745f3ac34
yjyoon-dev <yjyoon.dev@gmail.com>
2022-10-01 03:39:14 +0900

    feat: add NeuroGreen & Grey color

--- app/src/main/java/com/neurosky/zonetrainer/ui/theme/Color.kt
+++ app/src/main/java/com/neurosky/zonetrainer/ui/theme/Color.kt
@@ -4,7 +4,9 @@ import androidx.compose.ui.graphics.Color
 
 val NeuroBlue = Color(0xFF3AA2E3)
 val NeuroPurple = Color(0xFF6464D0)
+val NeuroGreen = Color(0xFF1FBF89)
 val NeuroRed = Color(0xFFFF6D6D)
 
-val Black = Color(0xFF000000)
+val Black = Color(0xFF303437)
+val Grey = Color(0xFF404446)
 val White = Color(0xFFFFFFFF)
