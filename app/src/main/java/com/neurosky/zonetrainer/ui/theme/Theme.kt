5eda0635d46fe7e7fce433061103a9f127f0a3f8
yjyoon-dev <yjyoon.dev@gmail.com>
2022-10-01 01:45:19 +0900

    feat: apply system ui settings on theme

--- app/src/main/java/com/neurosky/zonetrainer/ui/theme/Theme.kt
+++ app/src/main/java/com/neurosky/zonetrainer/ui/theme/Theme.kt
@@ -3,6 +3,9 @@ package com.neurosky.zonetrainer.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val LightColorScheme = lightColorScheme(
     primary = NeuroBlue,
@@ -27,6 +30,14 @@ private val LightColorScheme = lightColorScheme(
 fun NeuroTheme(
     content: @Composable () -> Unit
 ) {
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = true,
+            isNavigationBarContrastEnforced = false,
+        )
+    }
     MaterialTheme(
         colorScheme = LightColorScheme,
         typography = Typography,
