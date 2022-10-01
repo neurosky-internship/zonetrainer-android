83d20643f4d7241b52a9f2acada6cc09730a5e71
yjyoon-dev <yjyoon.dev@gmail.com>
2022-10-02 07:40:21 +0900

    feat: define ConnectingState of MindWave

---
+++ app/src/main/java/com/neurosky/zonetrainer/ui/model/ConnectingState.kt
@@ -0,0 +1,22 @@
+package com.neurosky.zonetrainer.ui.model
+
+import androidx.annotation.StringRes
+import com.neurosky.zonetrainer.R
+
+enum class ConnectingState(
+    @StringRes val displayStringRes: Int,
+    @StringRes val actionStringRes: Int
+) {
+    Searching(
+        displayStringRes = R.string.searching_msg,
+        actionStringRes = R.string.cancel
+    ),
+    Optimizing(
+        displayStringRes = R.string.optimizing_msg,
+        actionStringRes = R.string.cancel
+    ),
+    Disconnected(
+        displayStringRes = R.string.disabled_msg,
+        actionStringRes = R.string.back
+    )
+}
