1227b073b81ae282281f77903ce55cf6bb44b427
yjyoon-dev <yjyoon.dev@gmail.com>
2022-11-23 12:06:45 +0900

    feat: implement google sign in button

--- app/src/main/java/com/neurosky/zonetrainer/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/neurosky/zonetrainer/ui/splash/SplashScreen.kt
@@ -1,24 +1,47 @@
 package com.neurosky.zonetrainer.ui.splash
 
+import android.app.Activity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.neurosky.zonetrainer.R
-import com.neurosky.zonetrainer.ui.theme.NeuroTheme
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(
+    googleSignInClient: GoogleSignInClient,
+    isLoginButtonVisible: Boolean,
+    login: (String) -> Unit
+) {
+    val googleSignInActivity =
+        rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                if (result.data != null) {
+                    val account: GoogleSignInAccount =
+                        GoogleSignIn.getSignedInAccountFromIntent(intent).result
+
+                    login(account.id!!)
+                }
+            }
+        }
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -32,22 +55,41 @@ fun SplashScreen() {
                 .align(Alignment.Center)
                 .size(256.dp)
         )
-        Image(
-            painter = painterResource(id = R.drawable.img_brand),
-            contentDescription = null,
-            modifier = Modifier
-                .align(
-                    Alignment.BottomCenter
-                )
-                .width(96.dp)
-        )
-    }
-}
+        if (isLoginButtonVisible) {
+            ElevatedButton(
+                onClick = { googleSignInActivity.launch(googleSignInClient.signInIntent) },
+                colors = ButtonDefaults.elevatedButtonColors(containerColor = Color.White),
+                contentPadding = PaddingValues(horizontal = 24.dp, vertical = 12.dp),
+                modifier = Modifier.align(Alignment.BottomCenter)
 
-@Preview
-@Composable
-private fun SplashScreenPreview() {
-    NeuroTheme {
-        SplashScreen()
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_google),
+                        contentDescription = null,
+                        modifier = Modifier.width(24.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.sign_in_with_google),
+                        color = Color.Gray
+                    )
+                    Spacer(modifier = Modifier.width(24.dp))
+                }
+            }
+        } else {
+            Image(
+                painter = painterResource(id = R.drawable.img_brand),
+                contentDescription = null,
+                modifier = Modifier
+                    .align(
+                        Alignment.BottomCenter
+                    )
+                    .width(96.dp)
+            )
+        }
     }
 }
