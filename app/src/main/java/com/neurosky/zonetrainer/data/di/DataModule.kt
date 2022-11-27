fd9f266d973884d4fb7281d256f203d73374fe83
yjyoon-dev <yjyoon.dev@gmail.com>
2022-11-27 14:22:19 +0900

    feat: retrofit singleton provider

--- app/src/main/java/com/neurosky/zonetrainer/data/di/DataModule.kt
+++ app/src/main/java/com/neurosky/zonetrainer/data/di/DataModule.kt
@@ -1,4 +1,22 @@
 package com.neurosky.zonetrainer.data.di
 
-class DataModule {
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("http://localhost:3000")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 }
