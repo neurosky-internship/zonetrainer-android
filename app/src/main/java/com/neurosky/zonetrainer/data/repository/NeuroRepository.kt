ab19b260735425fd643201b7df72c17385626125
yjyoon-dev <yjyoon.dev@gmail.com>
2022-11-27 17:05:12 +0900

    feat: get HomeData on ViewModel via repository

---
+++ app/src/main/java/com/neurosky/zonetrainer/data/repository/NeuroRepository.kt
@@ -0,0 +1,72 @@
+package com.neurosky.zonetrainer.data.repository
+
+import com.neurosky.zonetrainer.data.remote.api.NeuroService
+import com.neurosky.zonetrainer.ui.home.HomeData
+import com.neurosky.zonetrainer.ui.model.NeuroType
+import com.patrykandpatryk.vico.core.entry.FloatEntry
+import javax.inject.Inject
+
+class NeuroRepository @Inject constructor(
+    private val service: NeuroService
+) {
+
+    suspend fun getHomeData(userId: String): Result<HomeData> = runCatching {
+        val recentDataResponse = service.getRecentData(userId)
+        val graphDataResponse = service.getGraphData(userId)
+
+        HomeData(
+            recentAttention = HomeData.RecentData(
+                type = NeuroType.Attention,
+                value = recentDataResponse.attention,
+                datetime = recentDataResponse.date ?: ""
+            ),
+            recentMeditation = HomeData.RecentData(
+                type = NeuroType.Meditation,
+                value = recentDataResponse.meditation,
+                datetime = recentDataResponse.date ?: ""
+            ),
+            attentionChart = HomeData.ChartData(
+                type = NeuroType.Attention,
+                maxEntryModel = List(7) { graphDataResponse.attentionData[it].maxData }.mapIndexed { x, y ->
+                    FloatEntry(
+                        x = x.toFloat(),
+                        y = y
+                    )
+                },
+                avgEntryModel = List(7) { graphDataResponse.attentionData[it].avgData }.mapIndexed { x, y ->
+                    FloatEntry(
+                        x = x.toFloat(),
+                        y = y
+                    )
+                },
+                minEntryModel = List(7) { graphDataResponse.attentionData[it].minData }.mapIndexed { x, y ->
+                    FloatEntry(
+                        x = x.toFloat(),
+                        y = y
+                    )
+                }
+            ),
+            meditationChart = HomeData.ChartData(
+                type = NeuroType.Meditation,
+                maxEntryModel = List(7) { graphDataResponse.meditationData[it].maxData }.mapIndexed { x, y ->
+                    FloatEntry(
+                        x = x.toFloat(),
+                        y = y
+                    )
+                },
+                avgEntryModel = List(7) { graphDataResponse.meditationData[it].avgData }.mapIndexed { x, y ->
+                    FloatEntry(
+                        x = x.toFloat(),
+                        y = y
+                    )
+                },
+                minEntryModel = List(7) { graphDataResponse.meditationData[it].minData }.mapIndexed { x, y ->
+                    FloatEntry(
+                        x = x.toFloat(),
+                        y = y
+                    )
+                }
+            )
+        )
+    }
+}
--- app/src/main/java/com/neurosky/zonetrainer/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/neurosky/zonetrainer/ui/home/HomeViewModel.kt
@@ -1,21 +1,22 @@
 package com.neurosky.zonetrainer.ui.home
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import com.neurosky.zonetrainer.data.repository.NeuroRepository
 import com.neurosky.zonetrainer.ui.base.BaseViewModel
 import com.neurosky.zonetrainer.ui.model.GoogleAccount
-import com.neurosky.zonetrainer.ui.model.NeuroType
-import com.patrykandpatryk.vico.core.entry.FloatEntry
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import java.time.LocalDateTime
+import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.random.Random
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    handle: SavedStateHandle
+    handle: SavedStateHandle,
+    repository: NeuroRepository
 ) : BaseViewModel() {
 
     val googleAccount = handle.get<GoogleAccount>(KEY_GOOGLE_ACCOUNT)!!
@@ -24,41 +25,15 @@ class HomeViewModel @Inject constructor(
     val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
     init {
-        _uiState.value = HomeUiState.Success(
-            data = HomeData(
-                recentAttention = HomeData.RecentData(
-                    type = NeuroType.Attention,
-                    value = 51,
-                    datetime = LocalDateTime.now()
-                ),
-                recentMeditation = HomeData.RecentData(
-                    type = NeuroType.Meditation,
-                    value = 49,
-                    datetime = LocalDateTime.now()
-                ),
-                attentionChart = HomeData.ChartData(
-                    type = NeuroType.Attention,
-                    maxEntryModel = getRandomEntries(50, 100),
-                    avgEntryModel = getRandomEntries(25, 75),
-                    minEntryModel = getRandomEntries(0, 50)
-                ),
-                meditationChart = HomeData.ChartData(
-                    type = NeuroType.Meditation,
-                    maxEntryModel = getRandomEntries(50, 100),
-                    avgEntryModel = getRandomEntries(25, 75),
-                    minEntryModel = getRandomEntries(0, 50)
-                )
-            )
-        )
-    }
-
-    private fun getRandomEntries(from: Int, until: Int) = List(size = 7) {
-        Random.nextInt(from, until).toFloat()
-    }.mapIndexed { x, y ->
-        FloatEntry(
-            x = x.toFloat(),
-            y = y,
-        )
+        _uiState.value = HomeUiState.Loading
+        viewModelScope.launch {
+            repository.getHomeData(googleAccount.id)
+                .onSuccess { _uiState.value = HomeUiState.Success(it) }
+                .onFailure {
+                    Log.e("error", it.stackTraceToString())
+                    _uiState.value = HomeUiState.Failure
+                }
+        }
     }
 
     companion object {
